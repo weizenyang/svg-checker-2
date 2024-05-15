1b35098e5e4b3bc516280f156ef61591713055b8
weizenyang <edmund@edify.ac>
2024-05-15 11:40:20 +0100

    Update

--- src/components/object-list.tsx
+++ src/components/object-list.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef} from 'react';
 import './object-list.css'
 
 interface ObjectListProps {
@@ -7,6 +7,7 @@ interface ObjectListProps {
 const ObjectList: React.FC<ObjectListProps> = (props) => {
   const [eventData, setEventData] = useState<any>([]);
   const [csvEventData, setCsvEventData] = useState<any>([]);
+  const currentSelected = useRef<any>(50000);
   useEffect(() => {
     const handleFileUpload = (event: CustomEvent) => {
       setEventData(eventData => [...eventData, event.detail])
@@ -16,6 +17,47 @@ const ObjectList: React.FC<ObjectListProps> = (props) => {
       setCsvEventData(csvEventData =>[...csvEventData, event.detail])
     };
 
+    function clamp(num, min, max) {
+      return num <= min 
+        ? min 
+        : num >= max 
+          ? max 
+          : num
+    }
+
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if(document.querySelector(".item-list").children.length > 0){
+        if(event.key == "ArrowDown"){
+          if(currentSelected.current == 50000){
+            currentSelected.current = 0;
+            currentSelected.current = clamp(currentSelected.current, 0, document.querySelector(".item-list").children.length)
+            document.querySelector(".item-list").children[currentSelected.current].click()
+            document.querySelector(".item-list").children[currentSelected.current].focus()
+          } else {
+            currentSelected.current++;
+            currentSelected.current = clamp(currentSelected.current, 0, document.querySelector(".item-list").children.length)
+            document.querySelector(".item-list").children[currentSelected.current].click()
+            document.querySelector(".item-list").children[currentSelected.current].focus()
+          }
+        }
+
+        if(event.key == "ArrowUp"){
+          if(currentSelected.current == 50000){
+            currentSelected.current = 0;
+            currentSelected.current = clamp(currentSelected.current, 0, document.querySelector(".item-list").children.length)
+            document.querySelector(".item-list").children[currentSelected.current].click()
+            document.querySelector(".item-list").children[currentSelected.current].focus()
+          } else {
+            currentSelected.current--;
+            currentSelected.current = clamp(currentSelected.current, 0, document.querySelector(".item-list").children.length)
+            document.querySelector(".item-list").children[currentSelected.current].click()
+            document.querySelector(".item-list").children[currentSelected.current].focus()
+          }
+        }
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown as EventListener)
 
     // Adding the event listener
     window.addEventListener('file-upload', handleFileUpload as EventListener);
@@ -26,23 +68,49 @@ const ObjectList: React.FC<ObjectListProps> = (props) => {
     return () => {
       window.removeEventListener('file-upload', handleFileUpload as EventListener);
       window.removeEventListener('file-upload-csv', handleCSVFileUpload as EventListener);
+      window.removeEventListener('keydown', handleKeyDown as EventListener);
     };
   }, []); // Empty dependency array means this effect runs only once on mount
 
-  const switchEvent = (e) => {
-    window.dispatchEvent(new CustomEvent('object-switch', { detail: e }))}
+  const switchEvent = (e, i, f) => {
+
+    if(document.querySelector(".selected")){
+      document.querySelector(".selected").classList.remove('selected')
+    }
+
+    window.dispatchEvent(new CustomEvent('object-switch', { detail: e }))
+    currentSelected.current = i;
+    console.log(f.target)
+    f.target.classList.add("selected")
+  }
 
   return (
     <ul className="item-list">
     {eventData.map((data, index) => {
-        const matchedCsv = csvEventData.find(csvData => csvData.simplifiedName === data.unitName);
-        return (
-          <li className="item-list-item" key={`item-${index}`} onClick={() => switchEvent(data.unitName)}>
-            <p className="main-text">{data.unitName} <span className="matched-csv">{matchedCsv ? matchedCsv.name : ""}</span></p>
+      const csvEvent = csvEventData.find(csvData => csvData.simplifiedName === data.unitName)
+      return (
+        <button className="item-list-item-wrapper">
+        <li className="item-list-item" key={`item-${index}`} onClick={(e) => {
+            e.stopPropagation()
+            switchEvent(data.unitName, index, e)
+          }}>
+            <p className="main-text">{data.unitName} {csvEvent && <span className="matched-csv">{csvEvent.name}</span>}</p>
             <p className="secondary-text">{data.fileName}</p>
-          </li>
-        );
-      })}
+        </li>
+        {csvEvent && <div className="export-csv" onClick={(e) => {
+          window.dispatchEvent(new CustomEvent("export-csv", {
+              detail: csvEventData.find(csvData => csvData.simplifiedName === data.unitName) ? csvEventData.find(csvData => csvData.simplifiedName === data.unitName).name : ""
+          }))
+        }}>
+          CSV
+        </div>}
+      </button>
+      )
+        
+    }
+    
+          
+      )}
   </ul>
   );
 }
