8da1849ec308a52cc3829a3a0a851ced98f5f0b9
weizenyang <edmund@edify.ac>
2024-10-18 14:55:16 +0100

    Add Name Updater

---
+++ src/store.js
@@ -0,0 +1,6 @@
+import { atom } from 'nanostores';
+
+export const updateValuesModal = atom(false);
+export const updateValuesList = atom([]);
+export const currentValuesList = atom([]);
+export const circlesToSpawn = atom([])
